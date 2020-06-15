88d00d44c8f7388d1b6e82a47789fc91e4f184d0
hebr3 <brook057@cougars.csusm.edu>
2020-06-14 23:09:28 -0700

    Working first pass. Still need to add code highlighting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as R from "ramda";
+import "./App.css";
 
-function App() {
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/github.css";
+
+hljs.registerLanguage("javascript", javascript);
+
+function rotate(char, offset) {
+  return String.fromCharCode(((char.charCodeAt(0) - 65 + offset) % 26) + 65);
+}
+
+function encrypt(msg, offset) {
+  msg = msg.toUpperCase().replace(/[\s|\r|\n]/g, "");
+  let cipher = "";
+  for (let i = 0; i < msg.length; i++) {
+    cipher += rotate(msg[i], offset);
+  }
+  return cipher;
+}
+
+const CODE = `
+function rotate(char, offset) {
+  return String.fromCharCode(((char.charCodeAt(0) - 65 + offset) % 26) + 65);
+}
+  
+function encrypt(msg, offset) {
+  msg = msg.toUpperCase().replace(/[\\s|\\r|\\n]/g, "");
+  let cipher = "";
+  for (let i = 0; i < msg.length; i++) {
+    cipher += rotate(msg[i], offset);
+  }
+  return cipher;
+}
+`;
+
+export default function App() {
+  const [plaintext, setPlaintext] = useState("Try entering some text");
+  const [ciphertext, setCiphertext] = useState("");
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    setCiphertext(encrypt(plaintext, offset));
+  }, [plaintext, offset]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Caesar Cipher</h3>
+      <label htmlFor="offset">Offset: </label>
+      <select id="offset" onChange={(evt) => setOffset(evt.target.value)}>
+        {R.range(0, 26).map((i) => (
+          <option key={i}>{i}</option>
+        ))}
+      </select>
+      <br />
+      <textarea
+        id="plaintext"
+        value={plaintext}
+        onChange={(evt) => setPlaintext(evt.target.value)}
+        rows="5"
+        cols="60"
+      />
+      <br />
+      <textarea
+        id="ciphertext"
+        value={ciphertext}
+        rows="5"
+        cols="60"
+        disabled
+      />
+      <pre>
+        <code className="javascript">{CODE}</code>
+      </pre>
     </div>
   );
 }
-
-export default App;
